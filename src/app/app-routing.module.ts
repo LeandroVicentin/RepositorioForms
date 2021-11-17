1f0d4223e85b76bba8bcbc9c5c0540a777008775
Leandro Vicentin <leandrovicentin94@gmail.com>
2021-11-17 00:57:16 -0300

    Commit Forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { FormThreeComponent } from './components/form-three/form-three.component';
+import { FormTwoComponent } from './components/form-two/form-two.component';
 import { FormOneComponent } from './components/form-one/form-one.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,7 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [
   { path: '', component : HeaderComponent},
-  { path: 'form', component : FormOneComponent}
+  { path: 'form', component : FormOneComponent},
+  { path: 'form2', component : FormTwoComponent},
+  { path: 'form3', component : FormThreeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormOneComponent } from './components/form-one/form-one.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormTwoComponent } from './components/form-two/form-two.component';
+import { FormThreeComponent } from './components/form-three/form-three.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormOneComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormTwoComponent,
+    FormThreeComponent
   ],
   imports: [
     BrowserModule,
